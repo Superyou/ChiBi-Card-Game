926f722e065e6c61b8966abcc0684c9780177bf9
shaoyuha <54727769+shaoyuha@users.noreply.github.com>
2020-02-13 21:03:52 -0800

    Updated
    Everything until section 42, most visual done

---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    int save = 0;
+	// New Game Button
+	public void PlayGame()
+	{
+		SceneManager.LoadScene ("BattleScene");	
+    }    
+
+	//Quit Button
+	public void QuitGame(){
+		Debug.Log("QUIT!");
+		Application.Quit();
+	}
+
+    public void LoadGame()
+    {
+
+        SceneManager.LoadScene(save);
+     
+    }
+
+    public void ReturnMain()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
